944150897e948a3aeaf93f93bc166c32d26c0234
Paul Kassar <paulkassar@gmail.com>
2017-05-17 19:37:29 +0100

    Added new test

--- spec/Frame_spec.js
+++ spec/Frame_spec.js
@@ -7,7 +7,6 @@ describe("Frame", function(){
     frame = new Frame();
   });
 
-
   it ("inputs a score of 4 and 5", function(){
     frame.addBowls(4,5);
     expect(frame.totalFrame).toEqual[(4,5)]
@@ -17,5 +16,9 @@ describe("Frame", function(){
     expect(function(){ frame.addBowls(8,5) }).toThrowError("There are only 10 pins");
   });
 
-  
+  it("checks if a strike is bowled ", function() {
+    frame.addBowls(10,0);
+    expect( frame.isStrike() ).toEqual(true)
+  });
+
 });
